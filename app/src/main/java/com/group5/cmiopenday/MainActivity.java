9b97cad0cf7db4c5d663f4c705e8ee0f200302eb
agrida2 <azzeddine_agrida@hotmail.com>
2019-04-01 20:32:45 +0200

    Mijn aanpassingen

--- app/src/main/java/com/group5/cmiopenday/MainActivity.java
+++ app/src/main/java/com/group5/cmiopenday/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button tempeduc = (Button)findViewById(R.id.temp);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOpenDayDetails();
+            }
+        });
 
         tempeduc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, EducationPage.class));
             }
         });
+            }
+            public void openOpenDayDetails(){
+        Intent intent = new Intent(this,openDayDetails.class);
+        startActivity(intent);
             }
 }
 
