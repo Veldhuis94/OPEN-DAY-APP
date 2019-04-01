58a043dcac1a319137342086d4b6e35a85742a9c
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-04-01 17:11:53 +0200

    created shortcut to education page
    because of lack of burger menu, yet to be implemented

--- app/src/main/java/com/group5/cmiopenday/MainActivity.java
+++ app/src/main/java/com/group5/cmiopenday/MainActivity.java
@@ -1,7 +1,10 @@
 package com.group5.cmiopenday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button tempeduc = (Button)findViewById(R.id.temp);
+
+        tempeduc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EducationPage.class));
+            }
+        });
+            }
 }
+
+
+
