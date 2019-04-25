3cb2df5ff853bc3e8d274f3591a0d08e2da4745d
bob <bob@bob.nl>
2019-04-25 14:09:24 +0200

    Added Floorplans and functionality for changing pics

--- app/src/main/java/com/group5/cmiopenday/FloorPlanActivity.java
+++ app/src/main/java/com/group5/cmiopenday/FloorPlanActivity.java
@@ -3,12 +3,50 @@ package com.group5.cmiopenday;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FloorPlanActivity extends AppCompatActivity {
 
+    Button Up;
+    Button Down;
+    TextView Floor;
+    int v ;
+    ImageView Image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_plan);
+
+        Up = (Button)findViewById(R.id.Floor_Up_Button);
+        Down = (Button)findViewById(R.id.Floor_Down_Button);
+        Floor = (TextView)findViewById(R.id.Floor_Value_Text);
+        Image = (ImageView)findViewById(R.id.FloorplanImageView);
+        Up.setOnClickListener(new View.OnClickListener() {
+        int imageResourceIds[] = {R.drawable.bg,R.drawable.first,R.drawable.second,R.drawable.third,R.drawable.fourth,R.drawable.fifth,R.drawable.sixth};
+            @Override
+            public void onClick(View view) {
+                if (v < 6) {
+                v += 1;
+                Floor.setText(String.valueOf(v));
+            }
+                Image.setImageResource(imageResourceIds[v]);
+            }
+        });
+        Down.setOnClickListener(new View.OnClickListener() {
+            int imageResourceIds[] = {R.drawable.bg,R.drawable.first,R.drawable.second,R.drawable.third,R.drawable.fourth,R.drawable.fifth,R.drawable.sixth};
+            @Override
+            public void onClick(View view) {
+                if(v > 0){
+                    v-=1;
+                    Floor.setText(String.valueOf(v));
+                }
+                Image.setImageResource(imageResourceIds[v]);
+            }
+        });
+
+
     }
 }
