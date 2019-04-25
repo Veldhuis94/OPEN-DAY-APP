53c9d8f5e04f68f617cc6c2c651772761d7da9ae
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-04-25 13:12:32 +0200

    Revert "Revert "initial pop up system (rough)""
    This reverts commit e5a4a3870d7d9ecda5dc0e32e1bae054ddca5e49.

--- app/src/main/java/com/group5/cmiopenday/openDayDetails.java
+++ app/src/main/java/com/group5/cmiopenday/openDayDetails.java
@@ -1,11 +1,15 @@
 package com.group5.cmiopenday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class openDayDetails extends AppCompatActivity {
 
@@ -20,7 +24,27 @@ public class openDayDetails extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageButton shareButton = findViewById(R.id.button2);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Y'all finna share";
+                String shareSub = "Shared from self-made android app, y'all proud of me?";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareSub);
+                startActivity(Intent.createChooser(myIntent, "Share using what?"));
+            }
+        });
 
+        Button popUpTest = findViewById(R.id.button3);
+        popUpTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PopActivity.class));
+            }
+        });
         drawer = findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
