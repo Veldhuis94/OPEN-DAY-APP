7b5435bac6e1d867daaa86b50ae306c8a570f465
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-04-26 13:10:09 +0200

    Java studypage
    Includes temporary button tho the parts of the app that we weren't able to access before without modifying AndroidManifest. Will make testing a lot easier.

--- app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
+++ app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
@@ -2,6 +2,7 @@ package com.group5.cmiopenday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StudyProgramsActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class StudyProgramsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_programs);
+
+        Button com = findViewById(R.id.sp_com);
+        com.setOnClickListener(new onClickStudypage(1, this));
+        Button cmd = findViewById(R.id.sp_cmd);
+        cmd.setOnClickListener(new onClickStudypage(2, this));
+        Button cmgt = findViewById(R.id.sp_cmgt);
+        cmgt.setOnClickListener(new onClickStudypage(4, this));
+        Button si = findViewById(R.id.sp_si);
+        si.setOnClickListener(new onClickStudypage(0, this));
+        Button ti = findViewById(R.id.sp_ti);
+        ti.setOnClickListener(new onClickStudypage(3, this));
     }
 }
