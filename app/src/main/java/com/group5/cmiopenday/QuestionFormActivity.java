2cc8f572dee2a8cbb1d4edc3f236098703814c36
agrida2 <azzeddine_agrida@hotmail.com>
2019-05-02 17:21:55 +0200

    Final
    Tested!!

--- app/src/main/java/com/group5/cmiopenday/QuestionFormActivity.java
+++ app/src/main/java/com/group5/cmiopenday/QuestionFormActivity.java
@@ -1,11 +1,9 @@
 package com.group5.cmiopenday;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,10 +22,6 @@ public class QuestionFormActivity extends menu_Activity{
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
         setTitle("Ask A Question");
 
 
