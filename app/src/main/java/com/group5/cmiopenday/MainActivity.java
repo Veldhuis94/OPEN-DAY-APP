798dbf8095d1a75c39519fd7b2e80c0b1cae480e
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-05-06 11:51:36 +0200

    Improve Imports

--- app/src/main/java/com/group5/cmiopenday/MainActivity.java
+++ app/src/main/java/com/group5/cmiopenday/MainActivity.java
@@ -1,15 +1,7 @@
 package com.group5.cmiopenday;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
