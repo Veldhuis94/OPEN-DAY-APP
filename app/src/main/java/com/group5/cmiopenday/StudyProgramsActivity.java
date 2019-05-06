6449abb76a0c101bc7b9ea13e162c847461d237d
Dinesh Pahladsingh <0966028@hr.nl>
2019-05-06 11:35:20 +0200

    error

--- app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
+++ app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
-=======
->>>>>>> 1322aa2ea681ef03a8a6b36569d3d0f96a28c858
 import android.widget.Button;
 
 public class StudyProgramsActivity extends AppCompatActivity {
@@ -20,14 +17,14 @@ public class StudyProgramsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.activity_education_page);
 
         btinformatica = (Button) findViewById(R.id.button7);
         btinformatica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( StudyProgramsActivity.this, Informatica_courseActivity.class));
+                startActivity(new Intent(StudyProgramsActivity.this, Informatica_courseActivity.class));
             }
         });
 
@@ -35,27 +32,26 @@ public class StudyProgramsActivity extends AppCompatActivity {
         btcommunicatie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( StudyProgramsActivity.this, Communicatie_courseActivity.class));
+                startActivity(new Intent(StudyProgramsActivity.this, Communicatie_courseActivity.class));
             }
         });
         btcmd = (Button) findViewById(R.id.button12);
         btcmd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( StudyProgramsActivity.this, cmd_courseActivity.class));
+                startActivity(new Intent(StudyProgramsActivity.this, cmd_courseActivity.class));
             }
         });
         bttinformatica = (Button) findViewById(R.id.button14);
         bttinformatica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( StudyProgramsActivity.this, Tinformatica_courseActivity.class));
+                startActivity(new Intent(StudyProgramsActivity.this, Tinformatica_courseActivity.class));
             }
         });
     }
 
-}
-=======
+
         setContentView(R.layout.activity_study_programs);
 
         int buttonIds[] = {R.id.sp_com, R.id.sp_cmd, R.id.sp_cmgt, R.id.sp_si, R.id.sp_ti};
@@ -66,4 +62,3 @@ public class StudyProgramsActivity extends AppCompatActivity {
         }
     }
 }
->>>>>>> 1322aa2ea681ef03a8a6b36569d3d0f96a28c858
