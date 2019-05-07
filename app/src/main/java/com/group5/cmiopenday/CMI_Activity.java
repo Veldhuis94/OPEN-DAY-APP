a984f306def5f7c26a33ba3b16ccf56ca1d5250d
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-05-07 14:30:11 +0200

    optimize imports

--- app/src/main/java/com/group5/cmiopenday/CMI_Activity.java
+++ app/src/main/java/com/group5/cmiopenday/CMI_Activity.java
@@ -1,7 +1,6 @@
 package com.group5.cmiopenday;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
+++ app/src/main/java/com/group5/cmiopenday/StudyProgramsActivity.java
@@ -1,6 +1,5 @@
 package com.group5.cmiopenday;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
