52b95dc3068fe57518f83d0ea4c200ef2a3819e7
LAPTOP-HTI3JQCG\audiv <audivangog@gmail.com>
2019-05-11 00:53:29 +0200

    Added validation + minor layout changes

--- app/src/main/java/com/group5/cmiopenday/QuestionFormActivity.java
+++ app/src/main/java/com/group5/cmiopenday/QuestionFormActivity.java
@@ -7,16 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.group5.cmiopenday.questionform.FormTextListener;
+import com.group5.cmiopenday.questionform.FormValidator;
+
 public class QuestionFormActivity extends menu_Activity{
 
+    FormValidator validator; //for validating the textfields
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionform);
         super.onCreateDrawer(savedInstanceState);
-
-
 
         final EditText email = (EditText) findViewById(R.id.questionform_editemail);
         final EditText subject = (EditText) findViewById(R.id.questionform_editsubject);
@@ -27,36 +29,29 @@ public class QuestionFormActivity extends menu_Activity{
         SendE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //String toS = to.getText().toString();
                 String toS = "HRotterdam12345@gmail.com";
 
                 String subS = subject.getText().toString();
-                String queS = question.getText().toString();
-                String nameS = name.getText().toString();
-                String emailS = email.getText().toString();
                 String messageS = question.getText().toString() + "\n\n Full Name: " + name.getText().toString() + "\n Email: " + email.getText().toString();
-
-
 
                 Intent email = new Intent(Intent.ACTION_SEND);
                 email.putExtra(Intent.EXTRA_EMAIL, new String[] {toS});
                 email.putExtra(Intent.EXTRA_SUBJECT, subS);
-                //email.putExtra(Intent.EXTRA_TEXT, queS);
                 email.putExtra(Intent.EXTRA_TEXT, messageS);
 
                 email.setType("message/rfc822");
                 startActivity(Intent.createChooser(email, "Choose app to send the Email"));
-
             }
         });
 
-
-
-
+        validator = new FormValidator(SendE, this); //initialise object for validating the fields
+        //create listeners for the validator, the validator will check the textfield after it has been changed.
+        email.addTextChangedListener(new FormTextListener(FormValidator.FieldType.Email, email, validator));
+        subject.addTextChangedListener(new FormTextListener(FormValidator.FieldType.Text, subject, validator));
+        question.addTextChangedListener(new FormTextListener(FormValidator.FieldType.Text, question, validator));
+        name.addTextChangedListener(new FormTextListener(FormValidator.FieldType.Text, name, validator));
 
     }
-
-
 }
 
 
---
+++ app/src/main/java/com/group5/cmiopenday/questionform/FormTextListener.java
@@ -0,0 +1,33 @@
+package com.group5.cmiopenday.questionform;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+//used to execute the form validator after changes of a textfield
+public class FormTextListener implements TextWatcher {
+    private FormValidator.FieldType type; //type of the textfield
+    private EditText textField; //textfield to validate
+    private FormValidator validator; //validator object to check the field
+
+    public FormTextListener(FormValidator.FieldType type, EditText textField, FormValidator validator){
+        this.type = type;
+        this.textField = textField;
+        this.validator = validator;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        validator.checkField(type, textField); //check if the field is valid.
+    }
+}
