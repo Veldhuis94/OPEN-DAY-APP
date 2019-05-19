a250d6537c474fc1c421a8cc1b57484a1b5e3ebb
LAPTOP-HTI3JQCG\audiv <audivangog@gmail.com>
2019-05-19 15:47:36 +0200

    Limit available apps

--- app/src/main/java/com/group5/cmiopenday/questionform/FormSender.java
+++ app/src/main/java/com/group5/cmiopenday/questionform/FormSender.java
@@ -2,6 +2,7 @@ package com.group5.cmiopenday.questionform;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 import com.group5.cmiopenday.R;
 
@@ -12,12 +13,14 @@ public class FormSender {
 
         String message = question + "\n\n Full Name: " + name + "\n Email: " + email;
 
-        Intent emailIntent = new Intent(Intent.ACTION_SEND); //Create an intent for sending this message
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO); //Create an intent for sending this message
+        //Explanation Unified Resource Identifier (URI): this class is used to tell the Content provider what data we want to access
+        //Content provider is responsible for sharing data with other applications, in this case, the email app of choice.
+        emailIntent.setData(Uri.parse("mailto:")); //create an URI object for emailing
         emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {receiverEmail}); //receiver email
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject); //subject
         emailIntent.putExtra(Intent.EXTRA_TEXT, message); //content of the message
 
-        emailIntent.setType("message/rfc822");
         context.startActivity(Intent.createChooser(emailIntent, context.getString(R.string.questionform_chooseApp))); //let the user choose his app for sending
     }
 }
