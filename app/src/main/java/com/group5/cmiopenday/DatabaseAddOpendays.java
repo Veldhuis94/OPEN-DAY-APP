1ff8e5c5c9415c015e4ee8cc58a8d90144272a36
agrida2 <azzeddine_agrida@hotmail.com>
2019-05-30 13:36:58 +0200

    First commit

---
+++ app/src/main/java/com/group5/cmiopenday/DatabaseAddOpendays.java
@@ -0,0 +1,13 @@
+package com.group5.cmiopenday;
+
+import android.os.Bundle;
+
+public class DatabaseAddOpendays extends menu_Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        super.onCreateDrawer(savedInstanceState);
+    }
+
+
+}
